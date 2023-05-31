dc91cdb1f7285844e636ed8f7edb9beaad4b291b
peterkyle01 <kylepeterkoine4@gmail.com>
2023-05-31 16:45:34 +0300

    Added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://origin-coffee-kenya.vercel.app/",
+  generateRobotsTxt: true, // (optional)
+
+};
